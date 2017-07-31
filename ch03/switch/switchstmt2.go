c41cf4bce844d5e7e5dc7013b6a77c83c2415185
Alejandro Guirao <lekumberri@gmail.com>
2017-07-31 09:24:35 +0200

    Add expressionless switch example

---
+++ ch03/switch/switchstmt2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Curr struct {
+	Currency string
+	Name     string
+	Country  string
+	Number   int
+}
+
+var currencies = []Curr{
+	Curr{"DZD", "Algerian Dinar", "Algeria", 12},
+	Curr{"AUD", "Australian Dollar", "Australia", 36},
+	Curr{"EUR", "Euro", "Belgium", 978},
+	Curr{"CLP", "Chilean Peso", "Chile", 152},
+}
+
+func find(name string) {
+	for i := 0; i < len(currencies); i++ {
+		c := currencies[i]
+		switch {
+		case strings.Contains(c.Currency, name),
+			strings.Contains(c.Name, name),
+			strings.Contains(c.Country, name):
+			fmt.Println("Found", c)
+		}
+	}
+}
+
+func main() {
+	find("Peso")
+	find("DZD")
+	find("KK")
+}

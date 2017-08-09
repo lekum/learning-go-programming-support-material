eaeabe3111a41851b7f7c8df5bb0cc4b340e21ab
Alejandro Guirao <lekumberri@gmail.com>
2017-08-09 09:15:44 +0200

    Add example of int literals

---
+++ ch04/ints/intslit.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	vals := []int{
+		1024,
+		0x0FF1CE,
+		0x8BADF00D,
+		0xBEEF,
+		0777,
+	}
+	for _, i := range vals {
+		fmt.Printf("Got %d\n", i)
+	}
+}

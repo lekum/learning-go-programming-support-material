2df8dd6674556b4615c85195d9ca303089376c34
Alejandro Guirao <lekumberri@gmail.com>
2017-10-26 11:32:54 +0200

    Add example of struct composition

---
+++ ch08/objects/structobj.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+type fuel int
+
+const (
+	GASOLINE fuel = iota
+	BIO
+	ELECTRIC
+	JET
+)
+
+type vehicle struct {
+	make  string
+	model string
+}
+
+type engine struct {
+	fuel   fuel
+	thrust int
+}
+
+func (e *engine) start() {
+	fmt.Println("Engine started.")
+}
+
+type truck struct {
+	vehicle
+	engine
+	axels  int
+	wheels int
+	class  int
+}
+
+func (t *truck) drive() {
+	fmt.Printf("Truck %s %s, on the go!\n", t.make, t.model)
+}
+
+type plane struct {
+	vehicle
+	engine
+	engineCount int
+	fixedWings  bool
+	maxAltitude int
+}
+
+func (p *plane) fly() {
+	fmt.Printf(
+		"Aircraft %s %s clear for takeoff!\n",
+		p.make,
+		p.model,
+	)
+}
+
+func main() {
+	p := plane{
+		vehicle{make: "Airbus", model: "A380"},
+		engine{fuel: fuel(1), thrust: 3},
+		2,
+		true,
+		300,
+	}
+	p.fly()
+	p.start()
+}

d660eb93fb4e1adeecd2f5d1845c0813bcf70507
Alejandro Guirao <lekumberri@gmail.com>
2017-11-16 08:02:40 +0100

    Add more examples with channels

---
+++ ch09/channels/chan3.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 4)
+	ch <- 2
+	ch <- 4
+	close(ch)
+	for i := 0; i < 4; i++ {
+		if val, opened := <-ch; opened {
+			fmt.Println(val)
+		} else {
+			fmt.Println("Channel closed!")
+		}
+	}
+}

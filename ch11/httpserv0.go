e12111ab838174dea40161f3003886dd0a56c521
Alejandro Guirao <lekumberri@gmail.com>
2018-01-11 07:50:18 +0100

    Add simple HTTP server

---
+++ ch11/httpserv0.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type msg string
+
+func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Add("Content-Type", "text/html")
+	resp.WriteHeader(http.StatusOK)
+	fmt.Fprint(resp, m)
+}
+
+func main() {
+	msgHandler := msg("Hello from high above!")
+	server := http.Server{Addr: ":4040", Handler: msgHandler}
+	server.ListenAndServe()
+}

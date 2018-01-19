d44fe34d8dcc4f8da1bd098684f421e7bcd1e615
Alejandro Guirao <lekumberri@gmail.com>
2018-01-19 09:25:45 +0100

    Add HTML interface

--- ch11/currency/jsonserv0.go
+++ ch11/currency/jsonserv0.go
@@ -3,12 +3,24 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	curr "github.com/vladimirvivien/learning-go/ch11/curr1"
 )
 
 var currencies = curr.Load("./data.csv")
+
+func gui(resp http.ResponseWriter, req *http.Request) {
+	file, err := os.Open("./currency.html")
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+	io.Copy(resp, file)
+}
 
 func currs(resp http.ResponseWriter, req *http.Request) {
 	var currRequest curr.CurrencyRequest
@@ -32,6 +44,7 @@ func currs(resp http.ResponseWriter, req *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", gui)
 	mux.HandleFunc("/currency", currs)
 
 	if err := http.ListenAndServe(":4040", mux); err != nil {
